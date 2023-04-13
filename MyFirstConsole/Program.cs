f17035cf1bdee2b2f31b7ad46ad40e5e09e34f2f
Mary Kariuki <marymwihaki123@gmail.com>
2023-04-13 14:33:53 -0500

    breaking code

--- MyFirstConsole/Program.cs
+++ MyFirstConsole/Program.cs
@@ -5,7 +5,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
         static void Main(string[] args)
+            
         {
+            int = "Not a number will cause error"
             Console.WriteLine("Hello World!");
         }
     }
